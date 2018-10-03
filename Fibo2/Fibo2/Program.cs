b601acbfa9bf0b16a803056c278de4856a91b92a
Elpoke12 <43417450+Elpoke12@users.noreply.github.com>
2018-10-02 20:41:51 -0700

    Add files via upload

---
+++ Fibo2/Fibo2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fibo2
+{
+    class Program
+	{
+		public static int Fibonacci(int n)
+		{
+			int a = 0;
+			int b = 1;
+			for (int i = 0; i < n; i++)
+			{
+				int temp = a;
+				a = b;
+				b = temp + b;
+			}
+			return a;
+		}
+
+		static void Main()
+		{
+            int op = 0;
+            Console.Write("Ingrese un numero: ");
+            for (int i = 0; i < op; i++)
+			{
+                Console.WriteLine(Fibonacci(op));
+
+			}
+			Console.ReadKey();
+		}
+    }
+}
